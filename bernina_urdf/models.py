decdeb804a1a8bd28adeca2645dfb4402fc3e1ad
Roman Mankowsky <hello@Romans-MBP-3.psi.ch>
2023-11-15 16:40:01 +0100

    added detector dummy to bernina robot

--- bernina_urdf/models.py
+++ bernina_urdf/models.py
@@ -3,6 +3,35 @@ from .utilities import Motion_Visualization
 from pathlib import Path
 basepath = Path(__file__).parent
 
+class Tx200_Ceiling_Linaxis(Robot):
+    def __init__(
+        self, 
+        vis=None,
+        ):
+
+        links, name, urdf_string, urdf_filepath = self.URDF_read(
+            basepath.joinpath("tx200_ceiling_linaxis.urdf"),
+            tld = basepath.as_posix(),
+            )
+
+        super().__init__(
+            links,
+            name=name,
+            manufacturer="Staeubli",
+            urdf_string=urdf_string,
+            urdf_filepath=None,
+            )
+        
+        self.sim = Motion_Visualization(
+            vis = vis,
+            robot = self,
+            camera_views = [
+                [[3.000,0,1.500], [0,0,1.500]],
+            ],
+            links = self.links
+            )
+
+
 class Tx200_Ceiling(Robot):
     def __init__(
         self, 
