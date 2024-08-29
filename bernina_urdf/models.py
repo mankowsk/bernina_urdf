f7c5aabb6d7cc11e9eea5c7a1678929896247192
Mathias Sander <mathias.sander@psi.ch>
2024-08-29 12:08:26 +0200

    added gps

--- bernina_urdf/models.py
+++ bernina_urdf/models.py
@@ -42,6 +42,40 @@ class Tx200_Ceiling(Robot):
             )
 
 @conversions
+class GPS(Robot):
+    def __init__(
+        self,
+        vis=None,
+        jf_id = "",
+        ):
+        if "07" in jf_id:
+            jf_id = "_16M"
+        else:
+            jf_id = ""
+        links, name, urdf_string, urdf_filepath = self.URDF_read(
+            basepath.joinpath(f"gps_phi_stage_hex.urdf"),
+            tld = basepath.as_posix(),
+            )
+
+        super().__init__(
+            links,
+            name=name,
+            manufacturer="Staeubli",
+            urdf_string=urdf_string,
+            urdf_filepath=None,
+            )
+
+        self.sim = Motion_Visualization(
+            vis = vis,
+            robot = self,
+            camera_views = [
+                [[-3.000,0,1.500], [0,0,1.500]],
+            ],
+            links = self.links
+            )
+
+
+@conversions
 class Tx200_Floor(Robot):
     def __init__(
             self,
